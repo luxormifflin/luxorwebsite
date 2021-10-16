11c4efbb2104d34d9c4673f6213a213fdf4c976a
CaroSalazarT <cst2615@gmail.com>
2021-10-16 00:17:44 -0500

    20211015 en reunion de equipo se empieza la autenticación de los usuarios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,38 +1,89 @@
 import axios from 'axios';
 
-export const obtenerProductos = async (setProductos, setEjecutarConsulta) => {
-    const options = { method: 'GET', url: 'https://localhost:5000/productos/' };
-    await axios
-        .request(options)
-        .then(function (response) {
-            setProductos(response.data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-    setEjecutarConsulta(false);
+const getToken = () => {
+    return `Bearer ${localStorage.getItem('token')}`;
+};
+
+export const obtenerProductos = async (successCallback, errorCallback) => {
+    const options = { method: 'GET', 
+    url: 'http://localhost:5000/productos/',
+    headers: {
+        Authorization: getToken(),
+    },
+};
+    await axios.request(options).then(successCallback).catch(errorCallback); 
+};
+
+export const crearProducto = async (data, successCallback, errorCallback) => {
+    const options = {
+        method: 'POST',
+        url: 'http://localhost:5000/productos/',
+        headers: {'Content-Type': 'application/json, Authorization: getToken()'},
+        data,
+    };
+    await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const editarProducto = async (id, data, successCallback, errorCallback) => {
+    const options = { 
+        method: 'PATCH',
+        url: `http://localhost:5000/productos/${id}/`,
+        headers: {'Content-Type':'aplication/json', Authorization: getToken() },
+        data,
+    };
+    await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const eliminarProducto = async (id, successCallback, errorCallback) => {
+    const options = {
+        method: 'DELETE',
+        url: `http://localhost:5000/productos/${id}/`,
+        headers: { 'Content-Type': 'application/json', Authorization: getToken() },
+    };
+    await axios.request(options).then(successCallback).catch(errorCallback);
 };
 
 // CRUD PARA USUARIOS
 
 export const obtenerUsuarios = async (successCallback, errorCallback) => {
-  const options = { method: 'GET', url: 'http://localhost:5000/usuarios' };
-  await axios.request(options).then(successCallback).catch(errorCallback);
+    const options = { 
+        method: 'GET', 
+        url: 'http://localhost:5000/usuarios',
+        headers: {
+            Authorization: getToken(),
+        } };
+    await axios.request(options).then(successCallback).catch(errorCallback);
 };
-
+export const obtenerDatosUsuario = async (successCallback, errorCallback) => {
+    const options = { 
+        method: 'GET', 
+        url: 'http://localhost:5000/usuarios/self', //ruta dummie, mi ruta, info personal
+        headers: {
+            Authorization: getToken(), //3. Enviarle el token al backend
+        } };
+    await axios.request(options).then(successCallback).catch(errorCallback);
+};
 // CRUD DE VENTAS
 
 export const crearVenta = async (data, successCallback, errorCallback) => {
-  const options = {
-    method: 'POST',
-    url: 'http://localhost:5000/ventas',
-    headers: { 'Content-Type': 'application/json' },
-    data,
-  };
-  await axios.request(options).then(successCallback).catch(errorCallback);
+    const options = {
+        method: 'POST',
+        url: 'http://localhost:5000/ventas',
+        headers: { 'Content-Type': 'application/json' },
+        data,
+    };
+    await axios.request(options).then(successCallback).catch(errorCallback);
 };
 
-export const obtenerVehiculos = async (successCallback, errorCallback) => {
-    const options = { method: 'GET', url: 'http://localhost:5000/vehiculos/' };
-    await axios.request(options).then(successCallback).catch(errorCallback);
-  };
+  // export const obtenerUsuarios = async (setVehiculos, setEjecutarConsulta = () => {}) => {
+  //   const options = { method: 'GET', url: 'http://localhost:5000/usuarios/' };
+  //   await axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       setVehiculos(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  //   setEjecutarConsulta(false);
+  // };
